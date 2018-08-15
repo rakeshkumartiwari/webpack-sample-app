f0930ba76799f61f7fb2d82ff275e6ddaac3467a
rakeshkumartiwari <tiwarirakesh8@gmail.com>
2018-08-15 18:09:36 +0530

    adding new project

---
+++ README.md
@@ -0,0 +1,10 @@
+# Welcome to Webpack 4 example
+
+This is an starter app for webpack 4
+
+## How to Get Going 
+* Clone the project 
+* Run `npm i`
+* Run `npm start`
+* For Build in production mode run `npm run build`
+* For Build in development mode run `npm run build:dev`
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = function () {
+    return {
+        entry: './js/app.js',
+        output: {
+            filename: 'bundle.js',
+            path: resolve(__dirname, 'dist')
+        },
+        context: resolve(__dirname, 'src'),
+        plugins: [
+            /** Html Webpack plugin helps to create index.html file for dev server */
+            new HtmlWebpackPlugin()
+        ]
+    }
+}
